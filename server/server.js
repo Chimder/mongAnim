ee04d1bdeae5fa6dadf26ed0424cc561b7584f10
Chimder <dimas.kirilyuk@gmail.com>
2023-09-22 12:26:31 +0300

    cors10

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(json());
 app.use(
   cors({
-    origin: ['http://localhost:3000/', 'https://animga.vercel.app/']
+    origin: ['http://localhost:3000', 'https://animga.vercel.app']
   })
 );
 app.use(router);
